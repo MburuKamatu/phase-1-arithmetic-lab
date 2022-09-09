557eda87991bc47d33e9d8c60359655d7ba00270
+ <+>
2022-09-09 11:12:49 +0300

    commit tree 80e6a140f1ea19909780395caa571f1b0ff4637c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
 
+/*function learning(num1,num2){
+    return multiply=num1*num2;
+}
+learning(31,2)*/
 // Write your code here
+
+const random=Math.floor(Math.random() * 10);
+let num3=24
+let num4=5
+let mod=num3%num4
+
+let max=Math.max(12,5,8,8,17,20)
